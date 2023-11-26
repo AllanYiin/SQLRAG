158a2916fc3a5249abdb5aa89bf3706ce92097ef
Allan <allan@datadecision.ai>
2023-11-26 14:06:59 +0800

    加入專案檔案。

---
+++ DistanceFunctions.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class DistanceFunctions
+{
+
+
+    [SqlFunction]
+    public static SqlDouble CosineSimilarity(SqlString vector1, SqlString vector2)
+    {
+        double[] vec1 = Array.ConvertAll(vector1.Value.Split(','), Double.Parse);
+        double[] vec2 = Array.ConvertAll(vector2.Value.Split(','), Double.Parse);
+
+        if (vec1.Length != vec2.Length)
+            return SqlDouble.Null;
+
+        double dotProduct = 0.0;
+        double normVec1 = 0.0;
+        double normVec2 = 0.0;
+
+        for (int i = 0; i < vec1.Length; i++)
+        {
+            dotProduct += vec1[i] * vec2[i];
+            normVec1 += vec1[i] * vec1[i];
+            normVec2 += vec2[i] * vec2[i];
+        }
+
+        if (normVec1 == 0.0 || normVec2 == 0.0)
+            return 0;
+
+        return dotProduct / (Math.Sqrt(normVec1) * Math.Sqrt(normVec2));
+    }
+
+
+    [SqlFunction]
+    public static SqlDouble EuclideanDistance(SqlString vector1, SqlString vector2)
+    {
+        double[] vec1 = Array.ConvertAll(vector1.Value.Split(','), Double.Parse);
+        double[] vec2 = Array.ConvertAll(vector2.Value.Split(','), Double.Parse);
+
+        if (vec1.Length != vec2.Length)
+            return SqlDouble.Null;
+
+        double sum = 0.0;
+        for (int i = 0; i < vec1.Length; i++)
+        {
+            sum += Math.Pow(vec1[i] - vec2[i], 2);
+        }
+
+        return Math.Sqrt(sum);
+    }
+
+    [SqlFunction]
+    public static SqlDouble ManhattanDistance(SqlString vector1, SqlString vector2)
+    {
+        double[] vec1 = Array.ConvertAll(vector1.Value.Split(','), Double.Parse);
+        double[] vec2 = Array.ConvertAll(vector2.Value.Split(','), Double.Parse);
+
+        if (vec1.Length != vec2.Length)
+            return SqlDouble.Null;
+
+        double sum = 0.0;
+        for (int i = 0; i < vec1.Length; i++)
+        {
+            sum += Math.Abs(vec1[i] - vec2[i]);
+        }
+
+        return sum;
+    }
+
+
+    [SqlFunction]
+    public static SqlDouble HammingDistance(SqlString vector1, SqlString vector2)
+    {
+        char[] vec1 = vector1.Value.ToCharArray();
+        char[] vec2 = vector2.Value.ToCharArray();
+
+        if (vec1.Length != vec2.Length)
+            return SqlDouble.Null;
+
+        int distance = 0;
+        for (int i = 0; i < vec1.Length; i++)
+        {
+            if (vec1[i] != vec2[i])
+                distance++;
+        }
+
+        return distance;
+    }
+
+    [SqlFunction]
+    public static SqlDouble ChebyshevDistance(SqlString vector1, SqlString vector2)
+    {
+        double[] vec1 = Array.ConvertAll(vector1.Value.Split(','), Double.Parse);
+        double[] vec2 = Array.ConvertAll(vector2.Value.Split(','), Double.Parse);
+
+        if (vec1.Length != vec2.Length)
+            return SqlDouble.Null;
+
+        double maxDifference = 0.0;
+        for (int i = 0; i < vec1.Length; i++)
+        {
+            double difference = Math.Abs(vec1[i] - vec2[i]);
+            if (difference > maxDifference)
+                maxDifference = difference;
+        }
+
+        return maxDifference;
+    }
+
+    [SqlFunction]
+    public static SqlDouble MinkowskiDistance(SqlString vector1, SqlString vector2, SqlDouble p)
+    {
+        if (p.Value < 1)
+            return SqlDouble.Null; // p 必須大於等於1
+
+        double[] vec1 = Array.ConvertAll(vector1.Value.Split(','), Double.Parse);
+        double[] vec2 = Array.ConvertAll(vector2.Value.Split(','), Double.Parse);
+
+        if (vec1.Length != vec2.Length)
+            return SqlDouble.Null;
+
+        double sum = 0.0;
+        for (int i = 0; i < vec1.Length; i++)
+        {
+            sum += Math.Pow(Math.Abs(vec1[i] - vec2[i]), p.Value);
+        }
+
+        return Math.Pow(sum, 1.0 / p.Value);
+    }
+}
+
+
---
+++ OpenaiFunction.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Data.SqlTypes;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using System.IO;
+using System.Reflection;
+
+
+public partial class OpenaiFunction
+{
+    private static string apikey = "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";  //此序號已經失效，請填入自己的序號
+
+    [SqlFunction]
+    public static SqlString GetEmbedding(SqlString inputText)
+    {
+        string apiKey = apikey;
+        string apiUrl = "https://api.openai.com/v1/embeddings"; 
+        string requestBody = $"{{\"model\":\"text-embedding-ada-002\",\"input\": \"{inputText}\",\"encoding_format\":\"float\"}}";
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        try
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+            request.Method = "POST";
+            request.Headers["Authorization"] = $"Bearer {apiKey}";
+            request.ContentType = "application/json";
+
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(requestBody);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                return new SqlString(ParseEmbedding(result)); // 返回結果
+            }
+        }
+        catch (Exception ex)
+        {
+            // 錯誤處理
+            return new SqlString($"Error: {ex.Message}");
+        }
+    }
+
+    [SqlFunction]
+    public static SqlString ChatCompletion(SqlString inputPrompt, SqlString systemProimpt = default(SqlString), SqlString model = default(SqlString))
+    {
+        string apiKey = apikey;
+        string apiUrl = "https://api.openai.com/v1/chat/completions";
+        string modelToUse = model.IsNull || String.IsNullOrWhiteSpace(model.Value) ? "gpt-3.5-turbo" : model.Value;
+        string requestBody = $@"{{""model"": ""{modelToUse}"", ""messages"": [{{""role"": ""user"", ""content"": ""{inputPrompt.ToString()}""}}]}}";
+
+        
+      
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        try
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+            request.Method = "POST";
+            request.Headers["Authorization"] = $"Bearer {apiKey}";
+            request.Headers["Accept-Language"] ="zh-TW";
+            request.ContentType = "application/json";
+
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(requestBody);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                return new SqlString(ParseChatting(result)); // 返回結果
+            }
+        }
+        catch (Exception ex)
+        {
+            // 錯誤處理
+            return new SqlString($"Error: {ex.Message}");
+        }
+    }
+
+
+    private static string ParseEmbedding(string jsonResponse)
+    {
+        // 簡單的 JSON 解析來提取 embedding
+        string startPattern = "\"embedding\": [";
+        string endPattern = "]";
+        int startIndex = jsonResponse.IndexOf(startPattern) + startPattern.Length;
+        int endIndex = jsonResponse.IndexOf(endPattern, startIndex);
+        string embeddingString = jsonResponse.Substring(startIndex, endIndex - startIndex);
+
+        // 清理和格式化數據
+        embeddingString = embeddingString.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+        return embeddingString;
+    }
+
+    private static string ParseChatting(string jsonResponse)
+    {
+        // 簡單的 JSON 解析來提取 embedding
+        string startPattern = "\"content\": ";
+        string endPattern = "finish_reason";
+        int startIndex = jsonResponse.IndexOf(startPattern) + startPattern.Length+1;
+        int endIndex = jsonResponse.IndexOf(endPattern, startIndex);
+        string responseString = jsonResponse.Substring(startIndex, endIndex - startIndex-17).TrimEnd(" ".ToCharArray());
+        //endIndex = responseString.IndexOf("\"   ", responseString.Length - 20);
+        //responseString= responseString.Substring(0, endIndex).TrimEnd(" ".ToCharArray());
+        // 清理和格式化數據
+        return responseString;
+    }
+
+}
